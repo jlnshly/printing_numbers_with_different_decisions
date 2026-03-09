d48b6358c1d589590c2d3c176c8b02fdf33e22f4
jlnshly <jelaineashleyramos@gmail.com>
2026-03-10 00:17:37 +0800

    Final code and algo

---
+++ printing_quotient_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Assign variable and input
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+#Code the operation
+print("Quotient: ", first_number / second_number)

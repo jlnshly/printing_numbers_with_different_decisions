7efe72a71d8fc745d8966a48384b35317ccd4ea5
jlnshly <jelaineashleyramos@gmail.com>
2026-03-10 00:13:54 +0800

    Final code

---
+++ printing_product_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Name variable
+#Assign user input
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+#Decide operation
+print("Product: ", first_number * second_number)

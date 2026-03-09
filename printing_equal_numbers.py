cb94cb6cb6f8cf4236f4d6de4a51af921d9cdaf0
jlnshly <jelaineashleyramos@gmail.com>
2026-03-09 20:28:59 +0800

    Final code and algo done

---
+++ printing_equal_numbers.py
@@ -0,0 +1,7 @@
+#Name the variables
+#Ask for user input
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+#Code the decision
+if first_number == second_number:
+    print("Equal")

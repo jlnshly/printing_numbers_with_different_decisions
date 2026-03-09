76a36df39d1f621f3ba328a2d618a418bb4e576c
jlnshly <jelaineashleyramos@gmail.com>
2026-03-09 20:34:31 +0800

    Final code

---
+++ printing_the_sum.py
@@ -0,0 +1,6 @@
+#Name the variable
+#Ask for input
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+#Code and print the operation
+print("Sum: ", first_number + second_number)

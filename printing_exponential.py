f5d093278ab2c1351c333bb8cbd84de4e759c349
jlnshly <jelaineashleyramos@gmail.com>
2026-03-10 00:20:03 +0800

    Final code done

---
+++ printing_exponential.py
@@ -0,0 +1,5 @@
+#Name variable and set input message
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+#Code the operation to be used
+print(first_number ** second_number)

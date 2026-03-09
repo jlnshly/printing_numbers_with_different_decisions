a36494212154b1c4d8a73798629b33e92d56f626
jlnshly <jelaineashleyramos@gmail.com>
2026-03-09 20:26:41 +0800

    Final code and algo

---
+++ printing_the_bigger_number.py
@@ -0,0 +1,9 @@
+#Name the variables
+#Asking users to input 2 numbers
+first_input = float(input("Enter first number: "))
+second_input = float(input("Enter second number: "))
+#Code the decision
+if first_input > second_input:
+    print(first_input)
+else:
+    print(second_input)

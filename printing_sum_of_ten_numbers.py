3d904a77d33bfb6c01cc493d233bc147b0ad8014
jlnshly <jelaineashleyramos@gmail.com>
2026-03-10 00:33:52 +0800

    Code done

---
+++ printing_sum_of_ten_numbers.py
@@ -0,0 +1,7 @@
+#Set the input message for ten times
+#Name the variable for the sum
+sum_of_numbers = 0
+for i in range(1,11):
+    numbers = float(input("Enter a number: "))
+    sum_of_numbers += numbers
+print(sum_of_numbers)

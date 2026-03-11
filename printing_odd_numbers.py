ac8b0702605f881b71ddeb5e04437f05680a413f
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 02:49:50 +0800

    set operation is not correct should be %2 == 1

--- printing_odd_numbers.py
+++ printing_odd_numbers.py
@@ -3,6 +3,6 @@
 odd_numbers = 0
 for i in range(1, 11):
     user_input = float(input(f"Enter a number: "))
-    if float(user_input) % 2 == 0:
+    if float(user_input) % 2 == 1:
         odd_numbers += 1
 print(odd_numbers)
